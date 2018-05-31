e88881cb8288a10feaade16d7efe345766df09fa
Bernie <bernie.wangbj@gmail.com>
2018-05-31 17:31:12 +0800

    remove incorrect warning message, rename file“

--- v4/templates/webpack.config.js
+++ v4/templates/webpack.config.js
@@ -5,13 +5,5 @@ const DEFAULT_ENV = 'dev';
 module.exports = function( cliEnv ) {
   let cliEnvConfig = allEnvConfigs[ cliEnv ];
 
-  if( !cliEnvConfig ) {
-    let avaibleEnvs = Object.keys( allEnvConfigs ).join(' ');
-    console.warn(`
-      Provided environment "${cliEnv}" was not found.
-      Please use one of the following ones: ${avaibleEnvs}
-    `);
-  }
-
   return cliEnvConfig || allEnvConfigs[ DEFAULT_ENV ];
 }
